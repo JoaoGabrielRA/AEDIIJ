b3478d5af4d12a8df29a9d2e634c2eb2668bf7cf
Lucas Morais Freire <83522974+Lucas-Morais-Freire@users.noreply.github.com>
2023-03-29 10:06:48 -0300

    initiated dev on CsvManager class
    added __init__, exploreData and findEntries methods.

---
+++ week2/refactor.py
@@ -0,0 +1,34 @@
+from csv import reader
+
+class CsvManager():
+    def __init__(self, filename):
+        f = open(filename, encoding='utf8')
+        read_f = list(reader(f))
+        self.header, self.data = read_f[0], read_f[1:]
+        f.close()
+    def exploreData(self, start, end, rows_and_columns=False):
+        dataset_slice = self.data[start:end]    
+        for row in dataset_slice:
+            print(row)
+        if rows_and_columns:
+            print('Number of rows:', len(self.data))
+            print('Number of columns:', len(self.header))
+    def printEntries(self, name):
+        for item in self.data:
+            if item[0] == name:
+                print(item)
+
+android = CsvManager('googleplaystore.csv')
+ios = CsvManager('AppleStore.csv')
+
+ios.exploreData(0, 10, True)
+
+print(android.data[10472])
+print(android.header)
+print(android.data[0])
+
+print(len(android.data))
+del android.data[10472]
+print(len(android.data))
+
+android.printEntries('Instagram')
